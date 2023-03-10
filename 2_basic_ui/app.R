a6255f390219b82984569700a811c8b4c6d33bd3
AliSaadatV <ali.saadat@epfl.ch>
2023-03-10 16:08:59 +0100

    3_basic_reactivity added

--- 2_basic_ui/app.R
+++ 2_basic_ui/app.R
@@ -1,11 +1,11 @@
 library(shiny)
 
 ui <- fluidPage(
-  
+  dataTableOutput("table")
 )
 
 server <- function(input, output, session) {
-  
+  output$table <- renderDataTable(mtcars, options = list(pageLength = 5))
 }
 
 shinyApp(ui, server)
